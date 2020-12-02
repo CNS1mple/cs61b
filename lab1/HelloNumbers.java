2c9966c9f35ee01777b3aa7c710189bf12e18ba0
GuangYang <cherishlsy.me@gmail.com>
2020-12-02 21:43:11 +0800

    completed lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0, sum = 0;
+        while (x < 9) {
+            System.out.print(sum + " ");
+            x = x + 1;
+	    sum += x;
+        }
+	System.out.println(sum);
+	System.out.println();
+    }
+}
