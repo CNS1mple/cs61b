67c20edcdd4f63e96b7c3fef9877ae6c8f261e21
path2go <cherishlsy.me@gmail.com>
2021-02-24 09:55:43 +0800

    TestOffByOne bug fix

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -12,5 +12,10 @@ public class TestOffByOne {
         assertEquals(false, offByOne.equalChars('o', 'o'));
         assertEquals(true, offByOne.equalChars('l', 'k'));
         assertEquals(false, offByOne.equalChars('A', 'a'));
+        assertEquals(true, offByOne.equalChars('A', 'B'));
+        assertEquals(true, offByOne.equalChars('B', 'A'));
+        assertEquals(false, offByOne.equalChars('B', 'a'));
+        assertEquals(true, offByOne.equalChars('a', 'b'));
+        assertEquals(true, offByOne.equalChars('&', '%'));
     }
 }
